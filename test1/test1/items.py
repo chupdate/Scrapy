0d4c730f294c42f42b4efe68567082bef3d89afb
LittleYou <chupdate@163.com>
2016-02-01 23:44:32 +0800

    111

--- test1/test1/items.py
+++ test1/test1/items.py
@@ -8,7 +8,10 @@
 import scrapy
 
 
-class Test1Item(scrapy.Item):
+class dmozItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    title=scrapy.Field()
+    url=scrapy.Field()
+    desc=scrapy.Field()
     pass
